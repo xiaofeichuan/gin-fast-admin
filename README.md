2f9dabc6fe55890928f929c866a9979a8eb2e532
chenkunwei <github.com>
2023-04-08 16:57:20 +0800

    异常处理/优化

--- README.md
+++ README.md
@@ -24,3 +24,34 @@ go mod init
 # 启动项目
 go run main.go
 ```
+
+Logo
+
+#口号：快速搭建权限管理系统、口号：快速、敏捷、免费、开源
+
+标签：
+
+后端：Go、Gin、Gorm
+
+前端：Vue3、TypeScript、Element Plus、vite、pinia
+
+
+
+#简介
+
+#在线体验
+
+#项目启动/使用说明
+
+#功能介绍
+
+#感谢
+
+#演示图片
+
+#支持一下
+
+
+
+
+
--- server/router/router.go
+++ server/router/router.go
@@ -9,30 +9,33 @@ import (
 
 // InitRouters 初始化路由
 func InitRouters() *gin.Engine {
-	Router := gin.Default()
+	routers := gin.Default()
 	//允许修复当前请求路径，如/FOO和/..//Foo会被修复为/foo，并进行重定向，默认为 false。
-	Router.RedirectFixedPath = true
+	routers.RedirectFixedPath = true
+
+	//异常处理
+	routers.Use(middleware.Recover)
 
 	//跨域
-	Router.Use(middleware.Cors())
+	routers.Use(middleware.Cors())
 
 	//实例化路由
 	systemRouter := adminRouter.SystemRouter{}
 	exampleRouter := exampleRouter.TestRouter{}
 
 	//授权路由
-	privateGroup := Router.Group("/")
+	privateGroup := routers.Group("/")
 	privateGroup.Use(middleware.JwtAuth()).Use(middleware.Casbin())
 	{
 		systemRouter.InitPrivateRouter(privateGroup) //系统路由
 	}
 
 	//公开路由
-	publicGroup := Router.Group("")
+	publicGroup := routers.Group("")
 	{
 		systemRouter.InitPublicRouter(publicGroup) //系统路由
 		exampleRouter.InitPublicRouter(publicGroup)
 	}
 
-	return Router
+	return routers
 }
